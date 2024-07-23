3a6f3e40d5ec7c8c0f941ea56aee396b63900e4b
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-23 13:29:05 +0200

    Refactor contact form submission logic for improved user experience and error handling

--- assets/javascripts/contact.js
+++ assets/javascripts/contact.js
@@ -15,13 +15,7 @@ function sendMail(event) {
   const subjectInput = form.querySelector("#subject");
 
   // Show the submit button
-  if (submitButton.textContent === "Send") {
-    console.log("sending...,true..");
-    submitButton.textContent = "sending...";
-    setTimeout(function () {
-      submitButton.textContent = "Sent!";
-    }, 2000); // 1000 milliseconds = 1 second
-  }
+
   if (
     nameInput.value === "" ||
     emailInput.value === "" ||
@@ -38,12 +32,18 @@ function sendMail(event) {
     }, 2000);
     return;
   }
+  if (submitButton.textContent.trimStart().trimEnd() === "Send") {
+    submitButton.textContent = "sending...";
+    setTimeout(function () {
+      submitButton.textContent = "Sent!";
+    }, 2000); // 1000 milliseconds = 1 second
+  }
   emailjs.sendForm("service_7rfvwkn", "template_2u09n2j", form).then(
     (response) => {
       successAlert.style.display = "block";
       setTimeout(() => {
         successAlert.style.display = "none";
-      }, 2000);
+      }, 4000);
       form.reset();
     },
     (error) => {
