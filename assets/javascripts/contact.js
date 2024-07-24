8e88d514a27e3fba217daaf1ac768be8e9cbca49
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-24 11:39:49 +0200

    Refactor contact form submission logic for improved user experience and error handling

--- assets/javascripts/contact.js
+++ assets/javascripts/contact.js
@@ -85,3 +85,105 @@ function optionSelected(selectElement) {
   const selectedValue = selectElement.value;
   document.getElementById("subject").value = selectedValue;
 }
+
+function showPopup(serviceName, serviceAmount) {
+  // Get the popup elements
+  const popup = document.getElementById("popup");
+  const popupServiceName = document.getElementById(
+    "service-form-popup-service-name"
+  );
+  const popupServiceAmount = document.getElementById(
+    "service-form-popup-service-amount"
+  );
+  popupServiceName.textContent = serviceName;
+  popupServiceAmount.textContent = serviceAmount;
+
+  const formResponse = document.getElementById("service-form-popup-response");
+  popup.style.display = "block";
+
+  // Add event listener to the form
+  const form = document.getElementById("service-form-popup-form");
+  form.addEventListener("submit", (event) =>
+    handleFormSubmit(event, serviceName, serviceAmount, formResponse)
+  );
+}
+
+function handleFormSubmit(event, serviceName, serviceAmount, formResponse) {
+  event.preventDefault(); // Prevent the default form submission
+  const form = document.getElementById("service-form-popup-form");
+
+  var isSuccess;
+  // Add form validation and submission
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("service-form-popup-email").value;
+    const phone = document.getElementById("service-form-popup-phone").value;
+    const serviceDateTime = document.getElementById(
+      "service-form-popup-date"
+    ).value;
+
+    isSuccess = false;
+    // Validate email, phone, and service date
+
+    // Form is valid, send the email
+    if (!validateEmail(email)) {
+      formResponse.textContent = "Please enter a valid email address.";
+      formResponse.style.color = "red";
+      return;
+    }
+
+    if (!validatePhone(phone)) {
+      formResponse.textContent =
+        "Please enter a valid phone number (10-14 digits).";
+      formResponse.style.color = "red";
+      return;
+    }
+    formResponse.textContent =
+      "Order submitted successfully, we will get back to you soon!";
+    formResponse.style.color = "green";
+    formResponse.style.display = "block";
+    setTimeout(() => {
+      formResponse.style.display = "none";
+      document.getElementById("popup").style.display = "none";
+      form.reset();
+    }, 4000);
+
+    form.querySelector("#service_name").value = serviceName;
+    form.querySelector("#service_amount").value = serviceAmount;
+    emailjs.sendForm("service_7rfvwkn", "template_92w3oc8", form).then(
+      (response) => {
+        formResponse.textContent =
+          "Order submitted successfully, we will get back to you soon!";
+        formResponse.style.color = "green";
+        formResponse.style.display = "block";
+        setTimeout(() => {
+          formResponse.style.display = "none";
+          document.getElementById("popup").style.display = "none";
+          form.reset();
+        }, 4000);
+      },
+      (error) => {
+        formResponse.textContent = "Order submission failed. Please try again.";
+        formResponse.style.color = "red";
+        formResponse.style.display = "block";
+        console.log("FAILED...", error);
+      }
+    );
+  });
+}
+
+function hidePopup() {
+  const popupElement = document.getElementById("popup");
+  popupElement.style.display = "none";
+}
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validatePhone(phone) {
+  const phoneRegex = /^\d{10,14}$/;
+  return phoneRegex.test(phone);
+}
